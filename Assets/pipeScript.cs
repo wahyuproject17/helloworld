eec06805da1252d84ab70b1b120da86ba3eee393
wahyuproject17 <wahyuaguz9@gmail.com>
2024-02-28 12:10:08 +0700

    upload

---
+++ Assets/pipeScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeScript : MonoBehaviour
+{
+    float nilai;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilai = Random.Range(-3.69f, 1.82f);
+        transform.position = new Vector2(transform.position.x, nilai);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left * 100, Time.deltaTime * 5);
+    }
+}
